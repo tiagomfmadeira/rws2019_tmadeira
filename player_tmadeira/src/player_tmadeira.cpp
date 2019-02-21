2e9b77df36b9f4bbac1c84a9ce64b47394ce9433
Tiago <negativespade@gmail.com>
2019-02-21 10:52:53 +0000

    second commit

--- player_tmadeira/src/player_tmadeira.cpp
+++ player_tmadeira/src/player_tmadeira.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::cout << "Hello, World!" << std::endl;
-	return 1;
+	// std::cout << "Hello, World!" << std::endl;
+	ros::init(argc, argv, "player_tmadeira");
+
+	ros::NodeHandle n;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        std::count << i << std::endl;
+    }
+
+	return 0;
 }
