1947a1f5247cfffc40d7b3f9517b7925b4a7d9be
Tiago <negativespade@gmail.com>
2019-02-21 10:56:57 +0000

    changed the print

--- player_tmadeira/src/player_tmadeira.cpp
+++ player_tmadeira/src/player_tmadeira.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <ros/ros.h>
 
 int main(int argc, char* argv[])
 {
-	// std::cout << "Hello, World!" << std::endl;
 	ros::init(argc, argv, "player_tmadeira");
 
 	ros::NodeHandle n;
 
+
     for (int i = 0; i < 10; ++i)
     {
-        std::count << i << std::endl;
+        std::cout << i << std::endl;
     }
 
 	return 0;
